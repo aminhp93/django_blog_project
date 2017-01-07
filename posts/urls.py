fa5c452f64acc560d7234e9d3db93f0a9341dbb4
aminhp93 <minhpn.org.ec@gmail.com>
2017-01-06 21:01:25 -0800

    url links and get absolute url

--- posts/urls.py
+++ posts/urls.py
@@ -4,7 +4,7 @@ from .views import (post_create, post_detail, post_list, post_update, post_delet
 
 urlpatterns = [
 	url(r'^create/$', post_create, name='create'),
-	url(r'^detail/$', post_detail, name='detail'),
+	url(r'^(?P<id>\d+)/$', post_detail, name='detail'),
 	url(r'^$', post_list, name='list'),
 	url(r'^update/$', post_update, name='update'),
 	url(r'^delete/$', post_detail, name='delete'),
--- posts/views.py
+++ posts/views.py
@@ -1,16 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 def post_create(request):
 	return HttpResponse("create")
 
-def post_detail(request):
-	return HttpResponse("detail")
+def post_detail(request, id):
+	# instance = Post.objects.get(id=5)
+	instance = get_object_or_404(Post, id = id)
+	context = {
+		"title": "detail",
+		"instance": instance
+	}
+	return render(request, 'post_detail.html', context)
 
 def post_list(request):
-	return HttpResponse("list")
+	queryset = Post.objects.all()
+
+	context = {
+			"object_list": queryset,
+			"title": "List"
+		}
+	# if request.user.is_authenticated():
+
+	# 	context = {
+	# 		"title": "My user list"
+	# 	}
+	# else:
+	# 	context = {
+	# 		"title": "List"
+	# 	}
+
+	return render(request, 'index.html', context)
 
 def post_update(request):
 	return HttpResponse("update")
