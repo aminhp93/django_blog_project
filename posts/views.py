94b6d662de9f0f64880196441ce6173efff0d4c8
aminhp93 <minhpn.org.ec@gmail.com>
2017-01-07 18:36:48 -0800

    basic user permission

--- posts/views.py
+++ posts/views.py
@@ -1,13 +1,26 @@
+try:
+	from urllib import quote_plus
+except:
+	pass
+
+try:
+	from urllib.parse import quote_plus
+except:
+	pass
+
+
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, Http404
 from .models import Post
 from .forms import PostForm
 
 # Create your views here.
 
 def post_create(request):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	form = PostForm(request.POST or None, request.FILES or None)
 	if form.is_valid():
 		instance = form.save(commit=False)
@@ -28,9 +41,11 @@ def post_create(request):
 def post_detail(request, slug=None):
 	# instance = Post.objects.get(slug=5)
 	instance = get_object_or_404(Post, slug = slug)
+	share_string = quote_plus(instance.content)
 	context = {
 		"title": "detail",
-		"instance": instance
+		"instance": instance,
+		"share_string": share_string,
 	}
 	return render(request, 'post_detail.html', context)
 
@@ -68,6 +83,8 @@ def post_list(request):
 
 
 def post_update(request, slug=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post, slug = slug)
 
 	form = PostForm(request.POST or None, request.FILES or None, instance = instance)
@@ -87,6 +104,8 @@ def post_update(request, slug=None):
 	return render(request, 'post_form.html', context)
 
 def post_delete(request, slug=None):
+	if not request.user.is_staff or not request.user.is_superuser:
+		raise Http404
 	instance = get_object_or_404(Post, slug=slug)
 	instance.delete()
 	messages.success(request, "Successfully deleted")
