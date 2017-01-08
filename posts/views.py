700e14af1fc8acddba4b9ce87d400bac3700bdbe
aminhp93 <minhpn.org.ec@gmail.com>
2017-01-07 20:13:53 -0800

    search posts

--- posts/views.py
+++ posts/views.py
@@ -14,6 +14,7 @@ from django.contrib import messages
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.utils import timezone
+from django.db.models import Q
 
 
 from .models import Post
@@ -59,6 +60,15 @@ def post_list(request):
 
 	queryset_list = Post.objects.all().order_by("-timestamp")
 	# queryset_list = Post.objects.filter(draft=False).filter(publish__lte=timezone.now())
+
+	query = request.GET.get("q")
+	if query:
+		queryset_list = queryset_list.filter(
+			Q(title__icontains=query) |
+			Q(content__icontains=query) |
+			Q(user__first_name__icontains=query) |
+			Q(user__last_name__icontains=query) 
+			).distinct()
 
 	paginator = Paginator(queryset_list, 3) # Show 25 contacts per page
 	page_request_var = "hello"
